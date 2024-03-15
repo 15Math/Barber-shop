64e84c65f5c2618f43a246a1f35d9d720aa19db8
15Math <matheus.loureiro1215@gmail.com>
2024-03-14 21:31:32 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <header>
+        <div className='header'>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div className='header-content'>
+           
+                      <div className='nav'>
+                        <a href="">Agendamento</a>
+                        <a href="">Serviços</a>
+                      </div>
+
+                      <div className='logo'>
+                      </div>
+
+                      <div className='nav'>
+                        <a href="">Contato</a>
+                        <a href="">Clientes</a>
+                      </div>
+                  
+                  </div>
+                </div>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </header>
+      <section>
+      
+        <div className='scheduling'>
+          <p>Tá afim de lançar aquela régua??!!</p>
+          <a href="">Agende já</a>
+        </div>
+        <div className='barber'>
+          <img src="src\assets\adult-barber-trimming-clients-hair-barbershop.jpeg" alt=""/>
+        </div>
+
+      </section>
+
+    </>
+  )
+}
+
+export default App            
