12694066658db9efd3bd0e7ed20ec11e1c32d4df
15Math <matheus.loureiro1215@gmail.com>
2024-03-20 11:09:35 -0300

    Solve inconsistents and start services section

--- src/App.jsx
+++ src/App.jsx
@@ -34,27 +34,37 @@ function App() {
           </div>
         </div>
       </header>
-      <section>
+      <section className='scheduling-sec'>
       
-        <div className='scheduling'>
+        <div className='scheduling-area'>
           <div>
               <p>Tá afim de lançar aquela régua??!!</p>
               <a href="">Agende já</a>
           </div>
         </div>
+
         <div className='barber'>
-          
           <div className='red'>
           </div>
           <div className='blue'>
           </div>
           <img src="src\assets\adult-barber-trimming-clients-hair-barbershop.jpeg" alt=""/>
-            
-         
         </div>
-
+        
       </section>
 
+      <div className='division'>
+        <div>
+        </div>
+      </div>
+
+      <section className='services-sec'>
+         <div className='services-title'>
+            <img src="src/assets/poste-de-barbeiro.png" />
+            <h1>--Nossos Serviços--</h1>
+            <img src="src/assets/poste-de-barbeiro.png" />
+         </div>
+      </section>
     </>
   )
 }
