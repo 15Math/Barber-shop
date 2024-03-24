c18ba54c7bb64bd12744ca68b81ff36794fd6848
Giovanna Barroso Coelho <matheus.loureiro1215@gmail.com>
2024-03-24 10:45:08 -0300

    new animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@
 import './App.css'
 
 function App() {
-
   return (
     <>
 
@@ -36,14 +35,14 @@ function App() {
       </header>
       <section className='scheduling-sec'>
       
-        <div className='scheduling-area'>
+        <div className='scheduling-area' data-aos="zoom-in-right"  >
           <div>
               <p>Tá afim de lançar aquela régua??!!</p>
               <a href="">Agende já</a>
           </div>
         </div>
 
-        <div className='barber'>
+        <div className='barber' data-aos="flip-right"  data-aos-duration="2000" >
           <div className='red'>
           </div>
           <div className='blue'>
@@ -60,19 +59,19 @@ function App() {
 
       <section className='services-sec'>
          <div className='services-title'>
-            <img src="src/assets/poste-de-barbeiro.png" />
+            <img src="src/assets/poste-de-barbeiro.png" data-aos="flip-right"/>
             <h1>--Nossos Serviços--</h1>
-            <img src="src/assets/poste-de-barbeiro.png" />
+            <img src="src/assets/poste-de-barbeiro.png" data-aos="flip-right"/>
          </div>
-         <div className='services'>
-          <div>
+         <div className='services' >
+          <div data-aos="fade-right" data-aos-duration="1000">
             <div>
               <img src="src\assets\barba.png" alt="" />
               <div className='service-type barba'>
                 <p>Barba e sombrancelha</p>
               </div>
             </div>
-            <div className='services-list'>
+            <div className='services-list' >
               <p>Corte Tesoura ------------------- R$25,00</p>
               <p>Corte Tesoura ------------------- R$25,00</p>
               <p>Corte Tesoura ------------------- R$25,00</p>
@@ -81,7 +80,7 @@ function App() {
               <p>Corte Tesoura ------------------- R$25,00</p>
             </div>
           </div>
-          <div>
+          <div data-aos="fade-left" data-aos-duration="1000">
             <div>
               <img src="src\assets\cabelo.png" alt="" />
               <div className='service-type cabelo'>
