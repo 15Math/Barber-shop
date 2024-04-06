b3c31bbe9efa8832e24c9acb1f552b57343ee389
15Math <matheus.loureiro1215@gmail.com>
2024-04-05 22:03:05 -0300

    Responsivity increase

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,21 @@
 
+import { useState } from "react"
 import "./styles.css"
+
 export default function Header(){
+
+   
+
     return(
         <header>
           <div>
             <div>
               <div>
                 <div>
+
                   <div className='header-content'>
            
-                      <div className='nav'>
+                      <div className="nav">
                         <a href="">Agendamento</a>
                         <a href="">Serviços</a>
                       </div>
@@ -17,11 +23,10 @@ export default function Header(){
                       <div className='logo'>
                       </div>
 
-                      <div className='nav'>
+                      <div className="nav">
                         <a href="">Contato</a>
                         <a href="">Clientes</a>
                       </div>
-                  
                   </div>
                 </div>
               </div>
